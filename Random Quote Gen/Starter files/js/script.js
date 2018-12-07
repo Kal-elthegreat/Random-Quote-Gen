5deec4d35c316f6e113943cf676929448cce9ed9
Kiel Lewis <kiel.lewis@gmail.com>
2018-12-07 10:47:47 -0500

    removed comments

--- Random Quote Gen/Starter files/js/script.js
+++ Random Quote Gen/Starter files/js/script.js
@@ -69,14 +69,4 @@ printQuote = () => {
 // }
 
 
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
